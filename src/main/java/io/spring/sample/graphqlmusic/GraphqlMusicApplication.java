6a94230517e3cbe6595c0e12de51f8baa4e8b768
Brian Clozel <bclozel@vmware.com>
2023-05-05 14:54:53 +0200

    Create new application on start.spring.io
    We are creating here a new application for Spring Boot 3.1.x with the
    following dependencies:
    
    * GraphQL
    * WebFlux
    * MongoDB Reactive
    * Actuator
    * Distributed Tracing
    * Zipkin
    * Spring Boot configuration processor

---
+++ src/main/java/io/spring/sample/graphqlmusic/GraphqlMusicApplication.java
@@ -0,0 +1,13 @@
+package io.spring.sample.graphqlmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/spring/sample/graphqlmusic/GraphqlMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.sample.graphqlmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
