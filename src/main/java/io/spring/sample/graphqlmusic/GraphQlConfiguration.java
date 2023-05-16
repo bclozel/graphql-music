e8803a14677ccf3206a5b26a7f1b0451dd906196
Brian Clozel <bclozel@vmware.com>
2023-05-16 16:12:40 +0200

    Add schema inspection
    This commit configures the schema inspection feature and prints the
    content of the report to the console. Given the state of schema and
    mappings at this stage, we should see:
    
    ```
    Unmapped fields: {Query=[userPlaylists, Playlist]}
    Unmapped registrations: {}
    Skipped types: []
    ```

--- src/main/java/io/spring/sample/graphqlmusic/GraphQlConfiguration.java
+++ src/main/java/io/spring/sample/graphqlmusic/GraphQlConfiguration.java
@@ -3,6 +3,7 @@ package io.spring.sample.graphqlmusic;
 import graphql.scalars.ExtendedScalars;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.graphql.GraphQlSourceBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
@@ -17,6 +18,12 @@ public class GraphQlConfiguration {
         return wiringBuilder -> wiringBuilder.scalar(ExtendedScalars.Date)
                 .scalar(ExtendedScalars.Url)
                 .scalar(DurationSecondsScalar.INSTANCE);
+    }
+
+
+    @Bean
+    GraphQlSourceBuilderCustomizer inspectionCustomizer() {
+        return source -> source.inspectSchemaMappings(report -> logger.info(report.toString()));
     }
 
 }
