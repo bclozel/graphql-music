63c105ebd2361b99693d8a4ba3c49c3e7a29f325
Brian Clozel <bclozel@vmware.com>
2023-05-16 20:05:52 +0200

    Add userPlaylists query mapping

--- src/main/java/io/spring/sample/graphqlmusic/tracks/PlaylistController.java
+++ src/main/java/io/spring/sample/graphqlmusic/tracks/PlaylistController.java
@@ -4,6 +4,7 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 @Controller
 public class PlaylistController {
@@ -19,4 +20,9 @@ public class PlaylistController {
         return this.playlistRepository.findById(id);
     }
 
+    @QueryMapping
+    public Flux<Playlist> userPlaylists(@Argument String authorName) {
+        return this.playlistRepository.findByAuthor(authorName);
+    }
+    
 }
