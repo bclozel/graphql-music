265d3fb848eda3f75e76d9e52a573f012aef3691
Brian Clozel <bclozel@vmware.com>
2023-05-17 12:35:52 +0200

    Add QueryMapping for "playlist"

---
+++ src/main/java/io/spring/sample/graphqlmusic/tracks/PlaylistController.java
@@ -0,0 +1,22 @@
+package io.spring.sample.graphqlmusic.tracks;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Mono;
+
+@Controller
+public class PlaylistController {
+
+    private final PlaylistRepository playlistRepository;
+
+    public PlaylistController(PlaylistRepository playlistRepository) {
+        this.playlistRepository = playlistRepository;
+    }
+
+    @QueryMapping
+    public Mono<Playlist> playlist(@Argument String id) {
+        return this.playlistRepository.findById(id);
+    }
+
+}
